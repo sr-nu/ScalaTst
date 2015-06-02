4742f292eab85fd861bade3e0697b056aa0968b7
Srikanth <srikanth.nutigattu@gmail.com>
2015-06-02 23:09:20 +0800

    First commit for Scala coursera course

---
+++ src/greeter/Hello.scala
@@ -0,0 +1,9 @@
+package greeter
+
+/**
+ * Created by srikanthnutigattu on 02/06/15.
+ */
+object Hello extends App{
+  println("Hello Scala")
+
+}
---
+++ src/testScalaWorksheet.sc
@@ -0,0 +1,6 @@
+println("hello scala")
+
+var x =5
+def increase(i: Int ) = i+1
+increase(x)
+
